03b38db7291ffa3748318439740a4ce4fd143ef8
Ian Irving <ian@falsepositives.com>
2023-11-14 21:39:19 -0500

    Add Vue app instance and data method

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,4 +1,24 @@
+/**
+ * Vue application instance.
+ * @typedef {Object} VueApp
+ * @property {Function} component - Vue component function.
+ * @property {Function} mount - Mounts the Vue app to a DOM element.
+ */
+
+/**
+ * Creates a new Vue application instance.
+ * @function
+ * @returns {VueApp} A new Vue application instance.
+ */
+/**
+ * The Vue application instance.
+ * @type {Object}
+ */
 const app = Vue.createApp({
+  /**
+   * Returns the data object for the Vue component.
+   * @returns {Object} The data object containing the plans array.
+   */
   data() {
     return {
       plans: ["The Single", "The Curious", "The Addict"],
